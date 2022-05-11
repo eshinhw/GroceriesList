27337450acd2574fd9fcd5b0c735ff0e70714314
Eddie Shin <eshinhw@gmail.com>
2022-05-11 01:45:49 -0400

    updated recyclerView

--- app/src/main/java/com/eddieshin/shoppinglist/ShoppingDao.kt
+++ app/src/main/java/com/eddieshin/shoppinglist/ShoppingDao.kt
@@ -1,14 +1,23 @@
 package com.eddieshin.shoppinglist
 
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
+import androidx.lifecycle.LiveData
+import androidx.room.*
 
 @Dao
 interface ShoppingDao {
 
     @Insert(onConflict=OnConflictStrategy.REPLACE)
-    fun upsert(item: ShoppingItem)
+    // if the item is already in the DB, we replace the old one with the new one
+    suspend fun upsert(item: ShoppingItem)
+    // mix of update and insert
+    // if parameter item is already in the database, update
+    // otherwise, insert item
 
-    fun delete(item: ShoppingItem)
+    @Delete()
+    suspend fun delete(item: ShoppingItem)
+
+    @Query("SELECT * FROM shopping_items")
+    fun getAllShoppingItems() : LiveData<List<ShoppingItem>>
+    // LiveData object will make the update process on RV really efficient
+
 }
---
+++ app/src/main/java/com/eddieshin/shoppinglist/ShoppingDatabse.kt
@@ -0,0 +1,31 @@
+package com.eddieshin.shoppinglist
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(
+    entities = [ShoppingItem::class],
+    version = 1
+)
+abstract class ShoppingDatabase : RoomDatabase() {
+    abstract fun getShoppingDao(): ShoppingDao
+
+    companion object {
+        @Volatile // one thread at a time
+        private var instance: ShoppingDatabase? = null
+
+        private val LOCK = Any()
+
+        // Everytime we create an instance ShoppingDatabase, it returns ShoppingDatabase instance.
+        // If an instance already exists,
+        operator fun invoke(context: Context) = instance ?: synchronized(LOCK) {
+            instance ?: createDatabase(context).also { instance = it }
+        }
+
+        private fun createDatabase(context: Context) =
+            Room.databaseBuilder(context.applicationContext,
+                ShoppingDatabase::class.java, "ShoppingDB.db").build()
+    }
+}
